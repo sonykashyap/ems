0b2f12ecf4944f3308dbc22a0909c20e79a96d04
Sony Kashyap <sonykashyap17@gmail.com>
2026-09-01 23:48:13 +0530

    chat bot functionality is in progress

---
+++ src/components/chatbot/Chatbot.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Chatbot = () => {
+  return (
+    <div className="fixed bottom-4 right-4 z-1 bg-white rounded-lg shadow-lg">
+        <div className="chat-header font-semibold bg-blue-50 p-2 rounded-t-lg">
+            Welcome to the Chatbot! How can I assist you today?
+        </div>
+        <div className="chat-body p-2 rounded-b-lg">
+            Hello this is sony
+        </div>
+    </div>
+  )
+}
+
+export default Chatbot;
